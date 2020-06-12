e1651ba6b9c8fe336a3c3edaae8b02d9b162c4d1
Alexandra Paxton <paxton.alexandra@gmail.com>
2020-06-11 21:56:42 -0400

    Create more effective parameter search file

--- scripts/03-data_analysis/crqa-create_radius_search_parameters-pac.r
+++ scripts/03-data_analysis/crqa-create_radius_search_parameters-pac.r
@@ -13,11 +13,7 @@
 source('./scripts/03-data_analysis/libraries_and_functions-pac.r')
 
 # prep workspace and libraries
-invisible(lapply(c('tseriesChaos',
-                   'nonlinearTseries',
-                   'crqa',
-                   'quantmod',
-                   'cowplot'),
+invisible(lapply(c('dplyr'),
                  require,
                  character.only = TRUE))
 
@@ -25,21 +21,77 @@ invisible(lapply(c('tseriesChaos',
 conversation_df = read.table('./data/pac-filtered_movement_data.csv',
                              sep=',',header=TRUE)
 
+#### 2. Create radius list ####
+
 # specify the radii that we'll check
 radius.list = seq(.01, 22, .01)
 
 # create parameter search
 radius_grid_search = expand.grid(radius.list,
                                  unique(conversation_df$participant_id),
-                                 unique(conversation_df$task)) %>%
+                                 unique(conversation_df$task),
+                                 unique(conversation_df$partner_type)) %>%
   rename(radius = Var1,
          participant_id = Var2,
-         task = Var3) %>%
+         task = Var3,
+         partner_type = Var4) %>%
   rownames_to_column(var = "job_number")
 
-# save to file
-write.table(x = radius_grid_search,
-            file="./data/crqa_parameters/crqa_radius_search.csv",
+#### 3. Combine with other parameters ####
+
+# read in the RQA parameters
+delay_df = read.table('./data/crqa_parameters/ami_calculations-pac.csv',
+                      sep=',', header=TRUE)
+embed_df = read.table('./data/crqa_parameters/embed_calculations-pac.csv',
+                      sep=',',header=TRUE)
+
+# join everything
+parameter_df = full_join(conversation_df, delay_df,
+                         by=c('participant_id',
+                              'partner_type',
+                              'task')) %>%
+  full_join(., embed_df,
+            by=c('participant_id',
+                 'partner_type',
+                 'task')) %>%
+  
+  # only take parameters for each
+  select(-ami.loc0, -ami.loc1, 
+         -embed.0, -embed.1,
+         -frame, -t, -movement, -condition,
+         -interlocutor, -participant_gender) %>%
+  distinct() %>%
+  
+  # remove anyone who doesn't have a dimension
+  drop_na(embed.selected)
+
+# combine with grid search
+radii_and_parameter_df = full_join(radius_grid_search,
+                                   parameter_df,
+                                   by=c('participant_id',
+                                        'partner_type',
+                                        'task'))
+
+#### 4. Save separate partners to file ####
+
+# create friend file
+friend_search_df = radii_and_parameter_df %>% ungroup() %>%
+  dplyr::filter(partner_type == "Friend")
+
+# create prof file
+prof_search_df = radii_and_parameter_df %>% ungroup() %>%
+  dplyr::filter(partner_type == "Prof")
+
+# save friend to file
+write.table(x = friend_search_df,
+            file="./data/crqa_parameters/crqa-radius_search-friend.csv",
+            sep=',',
+            col.names=TRUE,
+            row.names=FALSE)
+
+# save prof to file
+write.table(x = prof_search_df,
+            file="./data/crqa_parameters/crqa-radius_search-prof.csv",
             sep=',',
             col.names=TRUE,
             row.names=FALSE)
